b28e1e7c02d47051b26971bf59f7a40a486bd9f8
lamvii <ael-idri@e2r7p11.1337.ma>
2022-09-30 12:14:08 +0100

    load map

--- check_playermap.c
+++ check_playermap.c
@@ -6,7 +6,7 @@
 /*   By: ael-idri <ael-idri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/29 22:07:11 by ael-idri          #+#    #+#             */
-/*   Updated: 2022/09/29 22:53:57 by ael-idri         ###   ########.fr       */
+/*   Updated: 2022/09/30 12:12:52 by ael-idri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 int	max_width(char **str)
 {
-	int	i;
-	int	max;
+	int		i;
+	size_t	max;
 
 	i = 0;
 	max = 0;
@@ -28,7 +28,30 @@ int	max_width(char **str)
 	return (max);
 }
 
-bool	init_palyermap(t_data *data)
+char	*str_refine(char *str, int width)
+{
+	char	*spacestr;
+	char	*trim_nline;
+	char	*tmp;
+	int		i;
+
+	i = 0;
+	if (width - (int)ft_strlen(str) == 0)
+		return (ft_substr(str, 0, ft_strlen(str) - 1));
+	spacestr = (char *)malloc(sizeof(char) * (width - (int)ft_strlen(str)+ 1));
+	if (!spacestr)
+		return (NULL);
+	while (i < (width - (int)ft_strlen(str)))
+		spacestr[i++] = ' ';
+	spacestr[i] = '\0';
+	trim_nline = ft_substr(str, 0, ft_strlen(str) - 1);
+	tmp = ft_strjoin(trim_nline, spacestr);
+	free(trim_nline);
+	free(spacestr);
+	return (tmp);
+}
+
+bool	init_playermap(t_data *data)
 {
 	int	length;
 	int	width;
@@ -40,20 +63,21 @@ bool	init_palyermap(t_data *data)
 	data->map = (char **)malloc(sizeof(char *) * length);
 	if (!data->map)
 		return (false);
-	// case null !!
 	while (i < length)
 	{
-		data->map[i] = str_refine(data->o_map + 6 + i, width);
+		data->map[i] = str_refine(data->o_map[i + 6], width);
+		// printf("|%s|\n", data->map[i]);
 		if (!data->map[i])
 			return (false);
 		i++;
 	}
+	data->map[i] = NULL;
 	return (true);
 }
 
 bool	check_player_map(t_data *data)
 {
-	if (init_playermap(data))
+	if (!init_playermap(data))
 	{
 		printf("fail init map!!\n");
 		return (false);
